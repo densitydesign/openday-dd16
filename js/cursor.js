e2f66c3e43b7c3d9c2e49aa6f63a821256e166bb
sironij <54326791+sironij@users.noreply.github.com>
2021-02-07 00:07:25 +0100

    bella storia

--- js/cursor.js
+++ js/cursor.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
   // update position of cursor
   cursor.css('left', e.pageX-50).css('top', e.pageY-50);
 
-  const isLinkTag = target.is('a', 'p');
+  const isLinkTag = target.hasClass('circ') != target.hasClass('banner') != target.hasClass('button') != target.hasClass('button-light') != target.hasClass('id') != target.is('a') != target.is('img');
   const isHovered = cursor.hasClass('hoveredCursor');
 
   // toggle the cursor class if necessary
