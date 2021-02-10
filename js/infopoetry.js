0e13211b463f4c97d396e201832865614d092e7b
andrea <benedetti.a93@gmail.com>
2021-02-10 09:58:01 +0100

    images infopoetry fix

--- js/infopoetry.js
+++ js/infopoetry.js
@@ -22,7 +22,7 @@ d3.tsv(infopoems).then(data => {
 
     d3.select("#title").text(d.title)
     d3.select("#author").text(d.author)
-    d3.select("#preview").attr("src", `../images/infopoetry-covers/${d.image}`)
+    d3.select("#preview").attr("src", `./images/infopoetry-covers/${d.image}`)
     d3.select("#desc").text(d.desc)
     d3.select("#link").attr("href", d.link)
   });
